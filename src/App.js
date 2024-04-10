b010da1da243d45c517329825f80eee0853850ab
Jorey Papritz <joreyp@uw.edu>
2024-04-10 14:37:53 -0700

    Component folder created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App(props) {
+    return (
+      <Header />
+    );
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+return (
+<div>
+    This is a header placeholder!
+</div>
+    )
+};
+export default Header;
