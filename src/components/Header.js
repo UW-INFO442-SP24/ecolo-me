8143bb6ffc5c471331711be65885d58072733e9a
alexngu8 <115044587+alexngu8@users.noreply.github.com>
2024-04-15 14:57:04 -0700

    Navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+
 function Header() {
-return (
-<div>
-    This is a header placeholder!
-</div>
-    )
-};
+   
+    return (
+        <header>
+        <nav className="navbar">
+            <div className="project-name">
+            <img width="50" height="50" src="https://img.icons8.com/color/48/000000/hand-planting.png" alt="hand-planting" />
+            <span>ECO-<span className="challenge-text">CHALLENGE</span></span>
+            </div>
+
+
+            <ul className="navbar-links">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#find">FIND CHALLENGES</a></li>
+                <li><a href="#resources">MORE RESOURCES</a></li>
+            </ul>
+
+        </nav>
+        </header>
+    );
+}
+
 export default Header;
