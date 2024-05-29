f896a4df6e20152d2748a6c46d601adf5ffc381b
Michelle Odnert <modnert@uw.edu>
2024-05-28 21:13:39 -0700

    add resources to resource page

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -9,36 +9,35 @@ function Resources() {
             <ul>
                 <li><a href="https://www.iea.org/reports/fossil-fuels-consumption-subsidies-2022">Fossil Fuels Consumption Subsidies 2022 – Analysis</a></li>
                 <li><a href="https://sdgs.un.org/goals/goal12">UN Sustainable Development Goal 12</a></li>
+                <li><a href="https://www.footprintcalculator.org/home/en">Calculate your environmental impact!</a></li>
             </ul>
             <h2>Waste Management</h2>
             <ul>
-                {/* PLACEHOLDERS */}
-                <li><a href="https://www.iea.org/reports/fossil-fuels-consumption-subsidies-2022">Fossil Fuels Consumption Subsidies 2022 – Analysis</a></li>
-                <li><a href="https://sdgs.un.org/goals/goal12">UN Sustainable Development Goal 12</a></li>
+                <li><a href="https://portal.ct.gov/deep/reduce-reuse-recycle/climate-change/climate-change-and-waste#:~:text=Solid%20waste%20contributes%20directly%20to,our%20solid%20waste%20combustion%20facilities.">Climate Change and Waste - CT.gov</a></li>
+                <li><a href="https://www.usda.gov/media/blog/2022/01/24/food-waste-and-its-links-greenhouse-gases-and-climate-change">Food Waste and it's Link to Greenhouse Gases - USDA.gov</a></li>
             </ul>
             <h2>Sustainable Consumption</h2>
             <ul>
-                {/* PLACEHOLDERS */}
-                <li><a href="https://www.iea.org/reports/fossil-fuels-consumption-subsidies-2022">Fossil Fuels Consumption Subsidies 2022 – Analysis</a></li>
-                <li><a href="https://sdgs.un.org/goals/goal12">UN Sustainable Development Goal 12</a></li>
+                
+                <li><a href="https://www.enelgreenpower.com/learning-hub/energy-transition/impacts-sustainable-consumption">Impacts of Sustainable Consumption - Enel Green Power</a></li>
+                <li><a href="https://www.bbc.com/future/article/20200326-the-hidden-impact-of-your-daily-water-use">Hidden Impact of Your Daily Water Use - BBC</a></li>
             </ul>
             <h2>Reuse & Recyle</h2>
             <ul>
-                {/* PLACEHOLDERS */}
-                <li><a href="https://www.iea.org/reports/fossil-fuels-consumption-subsidies-2022">Fossil Fuels Consumption Subsidies 2022 – Analysis</a></li>
-                <li><a href="https://sdgs.un.org/goals/goal12">UN Sustainable Development Goal 12</a></li>
+                
+                <li><a href="https://nems.nih.gov/environmental-programs/pages/benefits-of-recycling.aspx">Environmental Benefits of Recycling - NIH.gov</a></li>
+                <li><a href="https://www.epa.gov/circulareconomy/us-recycling-system">The US Recycling System - EPA.gov</a></li>
             </ul>
             <h2>Responsible Spending</h2>
             <ul>
-                {/* PLACEHOLDERS */}
-                <li><a href="https://www.iea.org/reports/fossil-fuels-consumption-subsidies-2022">Fossil Fuels Consumption Subsidies 2022 – Analysis</a></li>
-                <li><a href="https://sdgs.un.org/goals/goal12">UN Sustainable Development Goal 12</a></li>
+                
+                <li><a href="https://hbr.org/2019/07/the-elusive-green-consumer">Green Consumers - Harvard Business Review</a></li>
+                <li><a href="https://www.un.org/en/academic-impact/consumerism-and-climate-change-how-choices-you-make-can-help-mitigate-effects">Consumerism and Climate Change - UN</a></li>
             </ul>
             <h2>Community</h2>
             <ul>
-                {/* PLACEHOLDERS */}
-                <li><a href="https://www.iea.org/reports/fossil-fuels-consumption-subsidies-2022">Fossil Fuels Consumption Subsidies 2022 – Analysis</a></li>
-                <li><a href="https://sdgs.un.org/goals/goal12">UN Sustainable Development Goal 12</a></li>
+                <li><a href="https://www.cbecal.org/issues/how-we-create-change/">How we create change - Communities for a Better Environment</a></li>
+                <li><a href="https://www.epa.gov/environmentaljustice/environmental-justice-showcase-communities-region">Environmental Justice Showcase Communities by Region - EPA.gov</a></li>
             </ul>
             <h2>SDG 12: Resposible Consumption and Production</h2>
             <YouTubeEmbed embedId="dn-hLQk49eA" />
